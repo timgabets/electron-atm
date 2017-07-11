6bc3fe98e108c751129ce6aadaf4304b24815369
Tim Gabets <tim@gabets.ru>
2017-07-11 15:22:30 +0300

    parser.getIncomingMessageLength()

--- spec/jasmine/ParserSpec.js
+++ spec/jasmine/ParserSpec.js
@@ -6,9 +6,39 @@ describe("Parser", function() {
     p = new Parser();
   });
 
-  describe("parse()", function(){
-      it("should be able to get proper empty message length", function() {
-        expect(p.parse('')).toEqual(true);
+  describe("getIncomingMessageLength()", function(){
+      it("should be able to get length of the empty message", function() {
+        expect(p.getIncomingMessageLength('')).toEqual(0);
+      });
+
+      it("should be able to parse the message length 1", function() {
+        expect(p.getIncomingMessageLength('\x00\x01x')).toEqual(1);
+      });
+
+      it("should be able to parse the message length 10", function() {
+        expect(p.getIncomingMessageLength('\x00\x0ax')).toEqual(10);
+      });
+
+      it("should be able to parse the message length 255", function() {
+        expect(p.getIncomingMessageLength('\x00\xffx')).toEqual(255);
+      });
+
+      it("should be able to parse the message length 256", function() {
+        expect(p.getIncomingMessageLength('\x01\x00x')).toEqual(256);
+      });
+
+      it("should be able to parse the message length 43981", function() {
+        expect(p.getIncomingMessageLength('\xab\xcdx')).toEqual(43981);
+      });
+
+      it("should be able to parse the message length 65535", function() {
+        expect(p.getIncomingMessageLength('\xff\xffx')).toEqual(65535);
+      });
+  });
+
+  describe("parseHostMessage()", function(){
+      it("should be able to parse 'Go out of service message'", function() {
+        expect(p.parseHostMessage('\x00\x0c10\x1c000\x1c000\x1c2')).toEqual(true);
       });
   });
 });
--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,19 @@
 function Parser(){
-
+	/**
+	 * [getIncomingMessageLength checks first two bytes of incoming message to get the length]
+	 * @param  {[type]} message [message to be checked]
+	 * @return {[type]}         [length of the message as decimal number]
+	 */
+	this.getIncomingMessageLength = function(message){
+		var len = parseInt(message.charCodeAt(0)) * 256 + parseInt(message.charCodeAt(1));
+		if(!isNaN(len))
+			return len
+		else
+			return 0;
+	};
 };
 
-Parser.prototype.parse = function(message){
+Parser.prototype.parseHostMessage = function(message){
  	return true;
 };
 

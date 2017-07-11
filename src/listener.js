b6f33ac10e235791c14261f3407539209a67b546
Tim Gabets <tim@gabets.ru>
2017-07-11 12:30:16 +0300

    Separate buttons for trxn request and Ready

--- src/listener.js
+++ src/listener.js
@@ -11,9 +11,16 @@ document.getElementById('connect').addEventListener('click', _ => {
   network.connect(host, port);
 });
 
-document.getElementById('send').addEventListener('click', _ => {
+document.getElementById('send-transaction-request').addEventListener('click', _ => {
   //ipc.send('network-send');
-  network.send();
+  var data = '11\x1C000\x1C\x1C\x1C12\x1C;4575270595153145=20012211998522600001?\x1C\x1CFA  G  A\x1C00000000\x1C4;5=72;8:?=742?;\x1C00000000000000000000000000000000\x1C00000000000000000000000000000000\x1C\x1C2005210000000000000000000000000000000000000000000000';
+  network.send(data);
+});
+
+document.getElementById('send-ready').addEventListener('click', _ => {
+  //ipc.send('network-send');
+  var data = '22\x1C000\x1C\x1C9'
+  network.send(data);
 });
 
 document.getElementById('disconnect').addEventListener('click', _ => {
--- src/network.js
+++ src/network.js
@@ -43,11 +43,9 @@ exports.connect = function(host, port){
  * [send description]
  * @return {[type]} [description]
  */
-exports.send = function(){
-  var data = '11\x1C000\x1C\x1C\x1C12\x1C;4575270595153145=20012211998522600001?\x1C\x1CFA  G  A\x1C00000000\x1C4;5=72;8:?=742?;\x1C00000000000000000000000000000000\x1C00000000000000000000000000000000\x1C\x1C2005210000000000000000000000000000000000000000000000';
+exports.send = function(data){
   //var data = '22\x1C000\x1C\x1C9'
   send(data);
-
   client.on('data', function(data) {
     trace.trace(data, '<< ' + data.length + ' bytes received:');
   });

f079248b2b445e13c909a76ddfeffb3147685727
Tim Gabets <tim@gabets.ru>
2017-07-11 16:59:15 +0300

    getState() and addState()

--- spec/jasmine/StatesSpec.js
+++ spec/jasmine/StatesSpec.js
@@ -6,9 +6,46 @@ describe("States", function() {
     s = new States();
   });
 
+  describe("parseState()", function(){
+      it("should parse valid state properly", function() {
+        var parsed = { 
+          number: '000', 
+          type: 'A', 
+          screen_number: '870', 
+          good_read_next_state: '500', 
+          error_screen_number: '128', 
+          read_condition_1: '002', 
+          read_condition_2: '002', 
+          read_condition_3: '002', 
+          card_return_flag: '001', 
+          no_fit_match_next_state: '127' 
+        };
+        expect(s.parseState('000A870500128002002002001127')).toEqual(parsed);
+      });
+  });
+
   describe("addState()", function(){
-      it("should return false if state is incorrect", function() {
+      it("should return false while passing empty data", function() {
         expect(s.addState('')).toEqual(false);
+      });
+
+      it("should add valid state", function() {
+        var parsed = { 
+          number: '000', 
+          type: 'A', 
+          screen_number: '870', 
+          good_read_next_state: '500', 
+          error_screen_number: '128', 
+          read_condition_1: '002', 
+          read_condition_2: '002', 
+          read_condition_3: '002', 
+          card_return_flag: '001', 
+          no_fit_match_next_state: '127' 
+        };
+
+        expect(s.addState('000A870500128002002002001127')).toEqual(true);
+        expect(s.getState('000')).toEqual(parsed);
+
       });
   });
 });
--- src/states.js
+++ src/states.js
@@ -1,5 +1,27 @@
 function States(){
 	this.states = {};
+
+	this.parseState = function(data){
+		var parsed = {};
+		parsed.number = data.substring(0, 3)
+		parsed.type = data.substring(3, 4)
+		
+		switch(parsed.type){
+			case 'A':
+				parsed.screen_number = data.substring(4, 7);
+				parsed.good_read_next_state = data.substring(7, 10)
+				parsed.error_screen_number = data.substring(10, 13)
+				parsed.read_condition_1 = data.substring(13, 16)
+				parsed.read_condition_2 = data.substring(16, 19)
+				parsed.read_condition_3 = data.substring(19, 22)
+				parsed.card_return_flag = data.substring(22, 25)
+				parsed.no_fit_match_next_state = data.substring(25, 28)
+				break;
+		}
+
+		return parsed;
+	}
+
 }
 
 States.prototype.getState = function(state_number){
@@ -7,7 +29,13 @@ States.prototype.getState = function(state_number){
 };
 
 States.prototype.addState = function(state){
-    return false;
+	var parsed = this.parseState(state);
+	if(state){
+		this.states[parsed.number] = parsed;
+    	return true;
+	}
+   	else
+   		return false;
 };
 
 module.exports = States

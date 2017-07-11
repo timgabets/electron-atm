d4e8ae83f02c3019b55e450aa0e2e45c70331a58
Tim Gabets <tim@gabets.ru>
2017-07-11 16:07:22 +0300

    parser.parseTransactionReply()

--- spec/jasmine/ParserSpec.js
+++ spec/jasmine/ParserSpec.js
@@ -37,15 +37,41 @@ describe("Parser", function() {
   });
 
   describe("parse())", function(){
-      it("should be able to parse 'Go out of service message'", function() {
-        var parsed = { message_class: 'Terminal Command', LUNO: '000', message_sequence_number: '000', command_code: 'Go out-of-service' };
+      it("should be able to parse 'Go out of service' message", function() {
+        var parsed = { 
+          message_class: 'Terminal Command', 
+          LUNO: '000', 
+          message_sequence_number: '000', 
+          command_code: 'Go out-of-service' 
+        };
         expect(p.parse('10\x1c000\x1c000\x1c2', 12)).toEqual(parsed);
+      });
+
+      it("should be able to parse 'Transaction reply' message", function() {
+        var parsed = { 
+          message_class: 'Transaction Reply Command', 
+          LUNO: '000', 
+          message_sequence_number: '', 
+          next_state: '133', 
+          transaction_serial_number: '0775', 
+          function_identifier: '9', 
+          screen_number: '064', 
+          message_coordination_number: '2', 
+          card_return_flag: '0', 
+          printer_flag: '0' 
+        };
+        expect(p.parse('40\x1c000\x1c\x1c133\x1c\x1c07759064\x1c200', 25)).toEqual(parsed);
       });
   });  
 
   describe("parseHostMessage()", function(){
       it("should be able to parse 'Go out of service message'", function() {
-        var parsed = { message_class: 'Terminal Command', LUNO: '000', message_sequence_number: '000', command_code: 'Go out-of-service' };
+        var parsed = { 
+          message_class: 'Terminal Command', 
+          LUNO: '000', 
+          message_sequence_number: '000', 
+          command_code: 'Go out-of-service' 
+        };
         expect(p.parseHostMessage('\x00\x0c10\x1c000\x1c000\x1c2')).toEqual(parsed);
       });
   });
--- src/parser.js
+++ src/parser.js
@@ -20,19 +20,29 @@ function Parser(){
      */
     this.parse = function (message, length){
         var splitted = message.split('\x1c')
-        var parsed = {};
         
         switch(splitted[0][0]){
             case '1':
-                parsed.message_class = 'Terminal Command';
-                break;
+                return this.parseTerminalCommand(splitted);
+            case '4':
+                return this.parseTransactionReply(splitted);
             default:
+                console.log('Unhandled message class' + splitted[0][0]);
                 break;
         }
+    };
 
+    /**
+     * [parseTerminalCommand description]
+     * @param  {[type]} splitted [description]
+     * @return {[type]}          [description]
+     */
+    this.parseTerminalCommand = function(splitted){
+        var parsed = {};
+        parsed.message_class = 'Terminal Command';
         parsed.LUNO = splitted[1];
         parsed.message_sequence_number = splitted[2];
-
+        
         switch(splitted[3][0]){
             case '1':
                 parsed.command_code = 'Go in-service';
@@ -45,7 +55,26 @@ function Parser(){
         }
         
         return parsed;
-    };
+    }
+
+    this.parseTransactionReply = function(splitted){
+        var parsed = {};
+        parsed.message_class = 'Transaction Reply Command';
+        parsed.LUNO = splitted[1];
+        parsed.message_sequence_number = splitted[2];
+
+        parsed.next_state = splitted[3]
+
+        parsed.transaction_serial_number = splitted[5].substring(0, 4)
+        parsed.function_identifier = splitted[5].substring(4, 5)
+        parsed.screen_number = splitted[5].substring(5, 8)
+
+        parsed.message_coordination_number = splitted[6].substring(0, 1)
+        parsed.card_return_flag = splitted[6].substring(1, 2)
+        parsed.printer_flag = splitted[6].substring(2, 3)
+
+        return parsed;
+    }
 };
 
 Parser.prototype.parseHostMessage = function(message){

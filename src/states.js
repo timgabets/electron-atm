8d378561f0c644afadeada1a6c37c7b5ff64b012
Tim Gabets <tim@gabets.ru>
2017-07-11 17:41:10 +0300

    states.getEntry()

--- src/states.js
+++ src/states.js
@@ -2,6 +2,21 @@ function States(){
     this.states = {};
 
     /**
+     * [getEntry get the state entry, e.g. state entry 3 is a substring of original state string from position 7 to position10 ]
+     * @param  {[type]} data  [state data to parse]
+     * @param  {[type]} entry [state entry to get]
+     * @return {[type]}       [3-bytes long state entry on success, null otherwise]
+     */
+    this.getEntry = function(data, entry){
+        if(entry > 0 && entry < 2)
+            return data.substring(3, 4);
+        else if (entry < 10)            
+            return data.substring(1 + 3 * (entry - 1), 4 + 3 * (entry - 1));
+
+        return null;
+    }
+
+    /**
      * [parseState description]
      * @param  {[type]} data [description]
      * @return {[type]}      [description]
@@ -9,16 +24,16 @@ function States(){
     this.parseState = function(data){
         var parsed = {};
         parsed.number = data.substring(0, 3)
-        parsed.type = data.substring(3, 4)
+        parsed.type = this.getEntry(data, 1);
         
         switch(parsed.type){
             // Card read state
             case 'A':
-                parsed.screen_number = data.substring(4, 7);
-                parsed.good_read_next_state = data.substring(7, 10)
-                parsed.error_screen_number = data.substring(10, 13)
-                parsed.read_condition_1 = data.substring(13, 16)
-                parsed.read_condition_2 = data.substring(16, 19)
+                parsed.screen_number = data.substring(4, 7);    // 2
+                parsed.good_read_next_state = data.substring(7, 10) // 3
+                parsed.error_screen_number = data.substring(10, 13)  // 4
+                parsed.read_condition_1 = data.substring(13, 16) // 5
+                parsed.read_condition_2 = data.substring(16, 19) // 6
                 parsed.read_condition_3 = data.substring(19, 22)
                 parsed.card_return_flag = data.substring(22, 25)
                 parsed.no_fit_match_next_state = data.substring(25, 28)

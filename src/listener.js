f3a3b42c660f1dc5e6685669dc2c07c20d88c702
Tim Gabets <tim@gabets.ru>
2017-07-12 16:19:46 +0300

    Card selection

--- src/listener.js
+++ src/listener.js
@@ -5,11 +5,14 @@ const ipc = electron.ipcRenderer
 
 document.getElementById('connect').addEventListener('click', _ => {
   ipc.send('network-connect', document.getElementById('host').value, document.getElementById('port').value);
+
 });
 
 document.getElementById('send-transaction-request').addEventListener('click', _ => {
   var opcode = document.getElementById('opcode-buffer').value;
-  var trxn_request = '11\x1C000\x1C\x1C\x1C12\x1C;4575270595153145=20012211998522600001?\x1C\x1C' + opcode + '\x1C00000000\x1C4;5=72;8:?=742?;\x1C00000000000000000000000000000000\x1C00000000000000000000000000000000\x1C\x1C2005210000000000000000000000000000000000000000000000';
+  var track2 = document.getElementById('selected-card').value;
+  
+  var trxn_request = '11\x1C000\x1C\x1C\x1C12\x1C' + track2 + '\x1C\x1C' + opcode + '\x1C00000000\x1C4;5=72;8:?=742?;\x1C00000000000000000000000000000000\x1C00000000000000000000000000000000\x1C\x1C2005210000000000000000000000000000000000000000000000';
   ipc.send('network-send', trxn_request);
 });
 
@@ -18,6 +21,3 @@ document.getElementById('send-ready').addEventListener('click', _ => {
   ipc.send('network-send', ready);  
 });
 
-document.getElementById('disconnect').addEventListener('click', _ => {
-  ipc.send('network-disconnect');
-});

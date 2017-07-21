67674c14523823fb0d81d7d7a6b689ad731eb5ca
Tim Gabets <tim@gabets.ru>
2017-07-21 09:27:26 +0300

    should build solicited specific command reject status message

--- spec/controllers/BuilderSpec.js
+++ spec/controllers/BuilderSpec.js
@@ -21,5 +21,23 @@ describe("Builder", function() {
       expect(b.build(object)).toEqual('22\x1C777\x1C\x1C9');
     });
 
+    it("should build solicited command reject status message", function() {
+      var object = {
+        message_class: 'Solicited',
+        message_subclass: 'Status',
+        status_descriptor: 'Command Reject',
+      };
+      expect(b.build(object)).toEqual('22\x1C777\x1C\x1CA');
+    });
+
+    it("should build solicited specific command reject status message", function() {
+      var object = {
+        message_class: 'Solicited',
+        message_subclass: 'Status',
+        status_descriptor: 'Specific Command Reject',
+      };
+      expect(b.build(object)).toEqual('22\x1C777\x1C\x1CC');
+    });
+
   });
 });

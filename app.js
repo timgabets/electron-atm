3ba8cfc678a612ec9d676306aea96af9fe5304c1
Tim Gabets <tim@gabets.ru>
2017-07-21 20:14:16 +0300

    jquery try me

---
+++ app.js
@@ -0,0 +1,36 @@
+var electron = require('electron') // http://electron.atom.io/docs/api
+var path = require('path')         // https://nodejs.org/api/path.html
+var url = require('url')           // https://nodejs.org/api/url.html
+
+var window = null
+
+// Wait until the app is ready
+electron.app.once('ready', function () {
+  // Create a new window
+  window = new electron.BrowserWindow({
+    // Set the initial width to 800px
+    width: 1350,
+    // Set the initial height to 600px
+    height: 560,
+    // Set the default background color of the window to match the CSS
+    // background color of the page, this prevents any white flickering
+    backgroundColor: "#D6D8DC",
+    // Don't show the window until it ready, this prevents any white flickering
+    show: false
+  })
+
+  // Open the DevTools.
+  //window.webContents.openDevTools()
+
+  // Load a URL in the window to the local index.html path
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Show window when page is ready
+  window.once('ready-to-show', function () {
+    window.show()
+  })
+})

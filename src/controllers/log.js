a4cd164fd4b0c186804246c80534044da8f2d0fa
Tim Gabets <tim@gabets.ru>
2017-07-24 14:35:10 +0300

    Scrolling the log down to the bottom

--- src/controllers/log.js
+++ src/controllers/log.js
@@ -2,6 +2,9 @@ function Log(){
   this.log = function(data){
     $( _ => {
       $('#log-output').append('\n' + data);
+
+      // Scroll the bar down to the bottom
+      $('#log-output').scrollTop($('#log-output')[0].scrollHeight);
     })
   }
 }

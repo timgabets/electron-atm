28b2540d4a3045aab42f5952b3b9e06c9c2f33ac
Tim Gabets <tim@gabets.ru>
2017-07-24 15:48:10 +0300

    Preventing page reload after "Connect" button pressed

--- window.js
+++ window.js
@@ -6,4 +6,8 @@ $(function () {
   $('#connect').on('click', _ => {
     ipc.send('connect-button-pressed', '127.0.0.1', 11032);
   });
+
+  $("#connection-data-form").submit(function(e) {
+    e.preventDefault();
+  });
 })

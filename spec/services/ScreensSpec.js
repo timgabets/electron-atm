20c4aeae0b735775465b3566d9f9ec240e12a2d9
Tim Gabets <tim@gabets.ru>
2017-07-25 12:57:07 +0300

    should parse Display Image File control

--- spec/services/ScreensSpec.js
+++ spec/services/ScreensSpec.js
@@ -13,5 +13,15 @@ describe("Screens", function() {
       };
       expect(s.parseScreen('778iddqd')).toEqual(parsed);
     });
+
+    it("should parse Display Image File control", function() {
+      var parsed = {
+        number: '000',
+        clear_screen: true,
+        display_image_files_control: true,
+        image_file: 'PIC000.jpg',
+      };
+      expect(s.parseScreen('000\x0c\x1bPEPIC000.jpg\x1b\x5c')).toEqual(parsed);
+    });
   });  
 });
--- src/services/screens.js
+++ src/services/screens.js
@@ -1,7 +1,43 @@
 function ScreensService(){
   this.parseScreen = function(data){
     var parsed = {};
-    parsed.number = data.substring(0, 3);
+    parsed.number = data.substr(0, 3);
+    var i = 3;
+
+    while(i < data.length){
+      if(data[i] === '\x0c'){
+        /**
+         * FF control character
+         * 
+         * Clears the screen and positions the cursor in the
+         * top left hand corner of the screen. Sets blinking to
+         * ‘off’ and resets foreground and background
+         * colours to their defaults. NCR recommends you
+         * use the FF (form feed) control character at the
+         * start of the screen following an idle screen. This
+         * ensures the idle sequence is stopped before the
+         * next screen is displayed.
+         */
+        parsed.clear_screen = true;
+        i++;
+        continue;
+      }
+
+      if(data.substr(i, 3) === '\x1bPE'){
+        /**
+         * Display Image Files Control
+         * This control code allows you to display pictures on the screen, using
+         * the following control string
+         */
+        parsed.display_image_files_control = true;
+        parsed.image_file = data.substr(i+3).split('\x1b\x5c')[0];
+
+        i += (parsed.image_file.length + ('\x1b\x5c').length);
+        continue;
+      }
+      
+      i++;
+    }
 
     return parsed;
   }

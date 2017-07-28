d7f2685ca48586a1244bc0e30147d515c22d414d
Tim Gabets <tim@gabets.ru>
2017-07-28 17:43:33 +0300

    v0.2.1 Handing network errors

--- src/controllers/network.js
+++ src/controllers/network.js
@@ -6,50 +6,62 @@ const Trace = require('../controllers/trace.js');
 const Log = require('../controllers/log.js');
 
 function Network(log) {
-    /**
-     * [getOutgoingMessageLength get the Message Header Length]
-     * @param  {[type]} data [binary data]
-     * @return {[type]}      [two-bytes length, e.g. '\x00\x0a' for 10-byte data]
-     */
-    this.getOutgoingMessageLength = function (data){
-      return String.fromCharCode(data.length / 256) + String.fromCharCode(data.length % 256);
-    };
+  this.trace = new Trace();
+  this.client = new net.Socket();
+  
+  /**
+   * [getOutgoingMessageLength get the Message Header Length]
+   * @param  {[type]} data [binary data]
+   * @return {[type]}      [two-bytes length, e.g. '\x00\x0a' for 10-byte data]
+   */
+  this.getOutgoingMessageLength = function (data){
+    return String.fromCharCode(data.length / 256) + String.fromCharCode(data.length % 256);
+  };
 
-    /**
-     * [send description]
-     * @param  {[type]} data [description]
-     * @return {[type]}      [description]
-     */
-    this.send = function (data){
-      var binary_data = Buffer(this.getOutgoingMessageLength(data) + data, 'binary');
-      this.client.write(binary_data);
-      this.trace.trace(binary_data, '>> ' + binary_data.length + ' bytes sent:');
-    };
+  /**
+   * [send description]
+   * @param  {[type]} data [description]
+   * @return {[type]}      [description]
+   */
+  this.send = function (data){
+    var binary_data = Buffer(this.getOutgoingMessageLength(data) + data, 'binary');
+    this.client.write(binary_data);
+    this.trace.trace(binary_data, '>> ' + binary_data.length + ' bytes sent:');
+  };
 
-    /**
-     * [connect description]
-     * @param  {[type]} host [description]
-     * @param  {[type]} port [description]
-     * @return {[type]}      [description]
-     */
-    this.connect = function(host, port){
-      this.client.connect(port, host, _ => {
-        log.info('<b>Connected to ' + host + ':' + port + '</b>');
-      });
-    };
-
-    this.trace = new Trace();
-    this.client = new net.Socket();
-
-    /**
-     * [network disconnect listener]
-     * @param  {[type]} )  [description]
-     * @return {[type]}   [description]
-     */
-    this.client.on('close', _ => {
-      //this.client.destroy();
-      log.info('<b>Connection closed</b>');
+  /**
+   * [connect description]
+   * @param  {[type]} host [description]
+   * @param  {[type]} port [description]
+   * @return {[type]}      [description]
+   */
+  this.connect = function(host, port){
+    log.info('Connecting to ' + host + ':' + port );
+    this.client.connect(port, host, _ => {
+      log.info('Connected');
     });
+  };
+
+
+  /**
+   * [network disconnect listener]
+   * @param  {[type]} )  [description]
+   * @return {[type]}   [description]
+   */
+  this.client.on('close', _ => {
+    //this.client.destroy();
+    log.info('Connection closed');
+  });
+
+  /**
+   * [network error handler]
+   * @param  {[type]} 'error' [description]
+   * @param  {[type]} (e      [exception]
+   * @return {[type]}         [description]
+   */
+  this.client.on('error', (e) => {
+    log.error(e);
+  });
 }
 
 /**

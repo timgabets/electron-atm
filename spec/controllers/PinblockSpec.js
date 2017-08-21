b34598db7d54dcf6452fff9ee6b91e8c1a0c8f14
Tim Gabets <tim@gabets.ru>
2017-08-21 15:28:59 +0300

    pinblock.encode_to_atm_format

--- spec/controllers/PinblockSpec.js
+++ spec/controllers/PinblockSpec.js
@@ -3,8 +3,24 @@ describe("Pinblock", function() {
   pinblock = new Pinblock();
 
   describe("get()", function(){
-    it("should get PIN 1234", function() {
+    it("should get pinblock for PIN length 4", function() {
       expect(pinblock.get('1234', '4000001234562000')).toEqual('041234FEDCBA9DFF');
+    });
+
+    it("should get pinblock for PIN length 5", function() {
+      expect(pinblock.get('92389', '4000001234562')).toEqual('0592789FFFEDCBA9');
+    });
+  });
+
+  describe("encode_to_atm_format()", function(){
+    it("should encode pinblock to ATM format", function() {
+      expect(pinblock.encode_to_atm_format('0123456789ABCDEF')).toEqual('0123456789:;=<>?');
+    });
+  });
+
+  describe("decode_from_atm_format()", function(){
+    it("should decode pinblock from ATM format", function() {
+      expect(pinblock.decode_from_atm_format('0123456789:;=<>?')).toEqual('0123456789ABCDEF');
     });
   });
 });
--- src/controllers/pinblock.js
+++ src/controllers/pinblock.js
@@ -6,7 +6,7 @@ function Pinblock(){
     // character (hexadecimal F). For example, for the 5-digit PIN 92389, the block is: 0592 389F FFFF FFFF
     var block1 = '0' + pin.length.toString() + pin;
     while(block1.length < 16)
-      block1 += 'F'
+      block1 += 'F';
 
     // Another 16-digit block is made from four zeros and the 12 right-most digits of the
     // account number, excluding the check digit. For example, for the 13-digit account number
@@ -14,7 +14,50 @@ function Pinblock(){
     var block2 = '0000' + cardnumber.substr(cardnumber.length - 12 - 1, 12);
 
     return xor(block1, block2);
-  }
+  };
+
+  /**
+   * [encode_to_atm_format description]
+   * @param  {[type]} pinblock [description]
+   * @return {[type]}          [description]
+   */
+  this.encode_to_atm_format = function(pinblock){
+    var atm_pinblock = '';
+
+    for (var i = 0; i < pinblock.length; i++){
+      switch(pinblock[i]){
+        case 'A':
+          atm_pinblock += ':'
+          break;
+
+        case 'B':
+          atm_pinblock += ';'
+          break;
+
+        case 'C':
+          atm_pinblock += '='
+          break;
+
+        case 'D':
+          atm_pinblock += '<'
+          break;
+
+        case 'E':
+          atm_pinblock += '>'
+          break;
+
+        case 'F':
+          atm_pinblock += '?'
+          break;
+        
+        default:
+          atm_pinblock += pinblock[i];
+          break;
+      }
+    }
+
+    return atm_pinblock;
+  };
 }
 
 module.exports = Pinblock;

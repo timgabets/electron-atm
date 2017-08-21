79d01a26c8414350088437106b87ea961f6128b3
Tim Gabets <tim@gabets.ru>
2017-08-21 14:40:39 +0300

    Added      src/controllers/des3.js

---
+++ spec/controllers/Des3Spec.js
@@ -0,0 +1,18 @@
+describe("DES3", function() {
+  var DES3 = require('../../src/controllers/des3.js');
+  des3 = new DES3();
+
+  describe("encrypt()", function(){
+    it("should encrypt zeroed data with zeroed key", function() {
+      expect(des3.encrypt('00000000000000000000000000000000', '00000000000000000000000000000000')).toEqual('8CA64DE9C1B123A78CA64DE9C1B123A7');
+    });
+
+    it("should encrypt random 16-byte data with random key", function() {
+      expect(des3.encrypt('323004E8FEEBBCF681F3A0B76C611736', 'F91D0CF0D56B5B98')).toEqual('62C28519F7005F51');
+    });
+
+    it("should encrypt random 32-byte data with random key", function() {
+      expect(des3.encrypt('323004E8FEEBBCF681F3A0B76C611736', 'A30CA3BC0CC0BE26CE844F2742934B14')).toEqual('C12FC53F91D0CF0D56B5B9813A32B854');
+    });
+  });
+});
---
+++ src/controllers/des3.js
@@ -0,0 +1,23 @@
+const des = require('node-cardcrypto').des;
+
+function DES3(){
+  this.encrypt = function(key, msg){
+    if( key.length !== 32) {
+      throw Error('Key length is invalid. Only double keys supported (length 16)');
+    }
+    
+    var keyLeft = key.substr(0, 16);
+    var keyRight = key.substr(16, 32);
+
+    var data = msg;
+    data = des.ecb_encrypt(keyLeft, data);
+    data = des.ecb_decrypt(keyRight, data);
+    data = des.ecb_encrypt(keyLeft, data);
+ 
+    result = des.ecb_encrypt(key, msg);
+
+    return result;
+  }
+}
+
+module.exports = DES3;

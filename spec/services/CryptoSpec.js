4f03f1b69b4d8c3d3c8dc36ba857f541d0a8f6c4
Tim Gabets <tim@gabets.ru>
2017-09-12 10:39:07 +0300

    Added      src/services/crypto.js

---
+++ spec/services/CryptoSpec.js
@@ -0,0 +1,142 @@
+describe("CryptoService", function() {
+  var CryptoService = require('../../src/services/crypto.js');
+  var log, settings, s;
+
+  beforeEach(function() {
+    log = {
+      info: function() {
+      }
+    };
+    
+    settings = {
+      get: function() {
+        return {};
+      },
+      set: function(value){
+      }
+    };
+
+    s = new CryptoService(settings, log);
+  });
+
+  describe('getTerminalKey() and getMasterKey()', function(){
+    beforeEach(function() {
+      s.setMasterKey('B6D55EABAD23BC4FD558F8D619A21C34');
+      s.setTerminalKey('DEADBEEFDEADBEEFDEADBEEFDEADBEEF');
+    });
+
+    it('should get terminal key', function(){
+      expect(s.getTerminalKey()).toEqual(['DEADBEEFDEADBEEFDEADBEEFDEADBEEF', '2AE358']);
+    });
+
+    it('should get master key', function(){
+      expect(s.getMasterKey()).toEqual(['B6D55EABAD23BC4FD558F8D619A21C34', '55531F']);
+    });
+  });
+
+/*
+  describe('setTerminalKey() and setMasterKey()', function(){
+    beforeEach(function() {
+      s.setMasterKey('B6D55EABAD23BC4FD558F8D619A21C34');
+      s.setTerminalKey('DEADBEEFDEADBEEFDEADBEEFDEADBEEF');
+      spyOn(settings, 'set');
+    });
+
+    it('should set terminal key', function(){
+      expect(s.getTerminalKey()).toEqual(['DEADBEEFDEADBEEFDEADBEEFDEADBEEF', '2AE358']);
+      s.setTerminalKey('B667E96A6D5C961CB667E96A6D5C961C');
+      expect(s.getTerminalKey()).toEqual(['B667E96A6D5C961CB667E96A6D5C961C', '900A01']);
+      expect(settings.set).toHaveBeenCalled();
+    });
+
+    it('should set master key', function(){
+      expect(s.getMasterKey()).toEqual(['B6D55EABAD23BC4FD558F8D619A21C34', '55531F']);
+      s.setMasterKey('D2C4E412AE89A92AD2C4E412AE89A92A');
+      expect(s.getMasterKey()).toEqual(['D2C4E412AE89A92AD2C4E412AE89A92A', '58E506']);
+      expect(settings.set).toHaveBeenCalled();
+    });
+  });
+
+  describe('getKeyCheckValue()', function(){
+    it('should return key check value', function(){
+      expect(s.getKeyCheckValue('DEADBEEFDEADBEEFDEADBEEFDEADBEEF')).toEqual('2AE358');
+    })
+  });
+
+  describe('dec2hex()', function(){
+    it('should convert decimal string to hex string', function(){
+      expect(atm.dec2hex('040198145193087203201076202216192211251240251237')).toEqual('28C691C157CBC94CCAD8C0D3FBF0FBED');
+    });
+
+    it('should convert decimal string to hex string', function(){
+      expect(atm.dec2hex('000001002003004005006007008009010011012013014015')).toEqual('000102030405060708090A0B0C0D0E0F');
+    });
+  });
+*/
+/*
+  describe('setCommsKey()', function(){
+    beforeEach(function() {
+      s.setMasterKey('B6D55EABAD23BC4FD558F8D619A21C34');
+      s.setTerminalKey('DEADBEEFDEADBEEFDEADBEEFDEADBEEF');
+    });
+
+    it('should change terminal PIN key', function(){
+      var data = {
+        message_class: 'Data Command',
+        LUNO: 000,
+        message_sequence_number: '000',
+        message_subclass: 'Extended Encryption Key Information',
+        modifier: 'Decipher new comms key with current master key',
+        new_key_length: '030',
+        new_key_data: '040198145193087203201076202216192211251240251237',
+      };
+    */
+      /*
+        new_key_data: '040198145193087203201076202216192211251240251237' is decimal representation of 28C691C157CBC94CCAD8C0D3FBF0FBED
+        28C691C157CBC94CCAD8C0D3FBF0FBED is 7B278B03B439DDCACF8B3333AC591BCA encrypted under B6D55EABAD23BC4FD558F8D619A21C34.
+       */
+/*
+      expect(s.getTerminalKey()).toEqual(['DEADBEEFDEADBEEFDEADBEEFDEADBEEF', '2AE358']);
+      expect(s.setCommsKey(data.new_key_data, data.new_key_length)).toBeTruthy()
+      expect(s.getTerminalKey()).toEqual(['7B278B03B439DDCACF8B3333AC591BCA', '41DD5C']);
+    })
+
+    it('should raise if master key is empty', function(){
+      var data = {
+        message_class: 'Data Command',
+        LUNO: 000,
+        message_sequence_number: '000',
+        message_subclass: 'Extended Encryption Key Information',
+        modifier: 'Decipher new comms key with current master key',
+        new_key_length: '030',
+        new_key_data: '040198145193087203201076202216192211251240251237',
+      };
+    */
+      /*
+        new_key_data: '040198145193087203201076202216192211251240251237' is decimal representation of 28C691C157CBC94CCAD8C0D3FBF0FBED
+        28C691C157CBC94CCAD8C0D3FBF0FBED is 7B278B03B439DDCACF8B3333AC591BCA encrypted under B6D55EABAD23BC4FD558F8D619A21C34.
+       */
+/*
+      s.setMasterKey(null) ;
+      expect(s.setCommsKey(data.new_key_data, data.new_key_length)).toBeFalsy()
+    })
+  });
+
+  describe('getEncryptedPIN()', function(){
+    beforeEach(function() {
+      s.PIN_buffer = '1234';
+      s.card = {number: '4000001234562000'};
+      s.setTerminalKey('DEADBEEFDEADBEEFDEADBEEFDEADBEEF');
+    });
+
+    it('should get encrypted PIN', function(){
+      expect(s.getEncryptedPIN()).toEqual('<3;:1>04=88654<4');
+    });
+
+    it('should return null if no terminal key', function(){
+      s.setTerminalKey(undefined);
+      expect(s.getEncryptedPIN()).toBeNull();
+    });    
+  });
+*/
+});
---
+++ src/services/crypto.js
@@ -0,0 +1,135 @@
+const des3 = require('node-cardcrypto').des;
+
+function CryptoService(settings, log){
+
+
+  /**
+   * [setMasterKey description]
+   * @param {[type]} key [description]
+   */
+  this.setMasterKey = function(key){
+    this.keys.master_key.key = key;
+    settings.set('master_key', key);
+  };
+
+    /**
+   * [setTerminalKey description]
+   * @param {[type]} key [description]
+   */
+  this.setTerminalKey = function(key){
+    this.keys.pin_key.key = key;
+    settings.set('pin_key', key);
+  };
+
+
+  /**
+   * [getKeyCheckValue description]
+   * @param  {[type]} key [description]
+   * @return {[type]}     [description]
+   */
+  this.getKeyCheckValue = function(key){
+    var kcv = des3.ecb_encrypt(key, '00000000000000000000000000000000');
+    if(kcv)
+      return kcv.substr(0, 6);
+    else
+      return null;
+  }
+
+  /**
+   * [getTerminalKey description]
+   * @return {[type]} [description]
+   */
+  this.getTerminalKey = function(){
+    return [this.keys.pin_key.key, this.getKeyCheckValue(this.keys.pin_key.key)];
+  };
+
+
+  /**
+   * [getMasterKey description]
+   * @return {[type]} [description]
+   */
+  this.getMasterKey = function(){
+    return [this.keys.master_key.key, this.getKeyCheckValue(this.keys.master_key.key)] ;
+  };
+
+
+
+  /**
+   * [dec2hex convert decimal string to hex string, e.g. 040198145193087203201076202216192211251240251237 to 28C691C157CBC94CCAD8C0D3FBF0FBED]
+   * @param  {[type]} dec_string [decimal string ]
+   * @return {[type]}            [hex string]
+   */
+  this.dec2hex = function (dec_string){
+    var hex_string = '';
+    for(var i = 0; i < dec_string.length; i += 3){
+      var chunk = parseInt(dec_string.substr(i, 3)).toString(16);
+      (chunk.length === 1) ? (hex_string = hex_string + '0' + chunk ) : hex_string += chunk;
+    }
+
+    return hex_string.toUpperCase();
+  }
+
+  /**
+   * [setCommsKey description]
+   * @param {[type]} key_decimal [description]
+   * @param {[type]} length      [description]
+   */
+  this.setCommsKey = function(key_decimal, length){
+    var comms_key = this.dec2hex(key_decimal);
+    var expected_key_length = parseInt(length, 16) / 1.5;
+
+    if(comms_key.length !== expected_key_length)
+    {
+      log.error('Key length mismatch. New key has length ' + comms_key.length + ', but expected length is ' + expected_key_length);
+      return false;
+    }
+
+    log.info('New comms key received: ' + comms_key);
+    if(!this.keys.master_key.key)
+    {
+      log.error('Invalid master key: ' + this.keys.master_key.key);
+      return false;
+    }
+
+    this.keys.pin_key.key = des3.ecb_decrypt(this.keys.master_key.key, comms_key);
+    log.info('New comms key value: ' + this.keys.pin_key.key);
+    settings.set('pin_key', this.keys.pin_key.key);
+    return true;
+  };
+
+  /**
+   * [getEncryptedPIN description]
+   * @return {[type]}           [description]
+   */
+  this.getEncryptedPIN = function(){
+    if(this.keys.pin_key.key){
+      log.info('Clear PIN block:     [' + this.pinblock.get(this.PIN_buffer, this.card.number) + ']')
+
+      var encrypted_pinblock = des3.ecb_encrypt(this.keys.pin_key.key, this.pinblock.get(this.PIN_buffer, this.card.number));
+      log.info('Encrypted PIN block: [' + encrypted_pinblock + ']');
+
+      var atm_pinblock = this.pinblock.encode_to_atm_format(encrypted_pinblock);
+      log.info('Formatted PIN block: [' + atm_pinblock + ']');
+      
+      return atm_pinblock;
+    } else
+    {
+      log.error('Terminal key is not set, unable to encrypt PIN block');
+      return null;
+    }
+  };
+
+  this.keys = {
+    master_key: {
+      key: '', 
+      check_value: ''},
+    pin_key: {
+      key: '', 
+      check_value: ''}
+  };
+
+  (settings.get('master_key')) ? this.setMasterKey(settings.get('master_key')) : this.setMasterKey('4BA59DC607B13AF49F3CD22CB2FDA11E');
+  (settings.get('pin_key')) ? this.setTerminalKey(settings.get('pin_key')) : this.setTerminalKey('1E9CA58EBE65FF4B6F339393142DA096');
+};
+
+module.exports = CryptoService;

f3ce5577e9300bce6810218c3f4c1b75c92ce437
Tim Gabets <tim@gabets.ru>
2017-09-25 09:04:14 +0300

    CursorService: get/set cursor position

---
+++ spec/services/CursorSpec.js
@@ -0,0 +1,77 @@
+describe("CursorService", function() {
+  var CursorService = require('../../src/services/cursor.js');
+  var c;
+
+  beforeEach(function() {
+    c = new CursorService();
+  });
+
+  describe('insertChar()', function(){
+    it('should init cursor position', function(){
+      expect(c.cursor_position).toBeUndefined();
+      c.initCursor();
+      expect(c.cursor_position).toEqual({'x': 0, 'y': 0});
+    });
+  });
+
+  describe('getCursorPosition()', function(){
+    it('should get left top cursor position', function(){
+      c.initCursor();
+      expect(c.getCursorPosition()).toEqual({'x': '@', 'y': '@'});
+    })
+
+    it('should get right top cursor position', function(){
+      c.cursor_position = {'x': 31, 'y': 0};
+      expect(c.getCursorPosition()).toEqual({'x': '?', 'y': '@'});
+    })
+
+    it('should get bottom left cursor position', function(){
+      c.cursor_position = {'x': 0, 'y': 15};
+      expect(c.getCursorPosition()).toEqual({'x': '@', 'y': 'O'});
+    })
+
+    it('should get bottom right cursor position', function(){
+      c.cursor_position = {'x': 31, 'y': 15};
+      expect(c.getCursorPosition()).toEqual({'x': '?', 'y': 'O'});
+    })
+
+    it('should get center cursor position', function(){
+      c.cursor_position = {'x': 15, 'y': 7};
+      expect(c.getCursorPosition()).toEqual({'x': 'O', 'y': 'G'});
+    })
+  });
+
+  describe('setCursorPosition()', function(){
+    it('should set cursor position to FK', function(){
+      c.initCursor();
+      c.setCursorPosition('FK');
+      expect(c.getCursorPosition()).toEqual({'x': 'K', 'y': 'F'});
+    });
+
+    it('should set cursor position to top left', function(){
+      c.initCursor();
+      c.setCursorPosition('@@');
+      expect(c.getCursorPosition()).toEqual({'x': '@', 'y': '@'});
+    });
+
+    it('should set cursor position to top right', function(){
+      c.initCursor();
+      c.setCursorPosition('@?');
+      expect(c.getCursorPosition()).toEqual({'x': '?', 'y': '@'});
+    });
+
+    it('should set cursor position to bottom left', function(){
+      c.initCursor();
+      c.setCursorPosition('O@');
+      expect(c.getCursorPosition()).toEqual({'x': '@', 'y': 'O'});
+    });
+
+    it('should set cursor position to bottom right', function(){
+      c.initCursor();
+      c.setCursorPosition('O?');
+      expect(c.getCursorPosition()).toEqual({'x': '?', 'y': 'O'});
+    });
+  })
+
+
+});
--- src/services/cursor.js
+++ src/services/cursor.js
@@ -1,5 +1,70 @@
+// X:
+var screen_columns = ['@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?'];
+// Y:
+var screen_rows = ['@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'];
+
 function CursorService(){
-  
+  this.cursor_position;
+
+  /**
+   * [initCursor description]
+   * @return {[type]} [description]
+   */
+  this.initCursor = function(){
+    this.cursor_position = {'x': 0, 'y': 0};
+  };
+
+  /**
+   * [getCursorPosition description]
+   * @return {[type]} [description]
+   */
+  this.getCursorPosition = function(){
+    return {
+      'x': screen_columns[this.cursor_position.x], 
+      'y': screen_rows[this.cursor_position.y]
+    };
+  }
+
+  /**
+   * [setCursorPosition description]
+   * @param {[type]} position_string [description]
+   */
+  this.setCursorPosition = function(position_string){
+    var y = position_string[0];
+    var x = position_string[1];
+
+    screen_columns.forEach( (element, i) => {
+      if(x === element)
+        this.cursor_position.x = i;
+    });
+
+    screen_rows.forEach( (element, i) => {
+      if(y === element)
+        this.cursor_position.y = i;
+    });
+  }
+
+  /**
+   * [moveCursor move screen cursor {count} positions to the right and carry to the next line if needed]
+   * @return {[type]} [description]
+   */
+  this.moveCursor = function(count){
+    if(!count)
+      count = 1;
+
+    this.cursor_position.y += Math.floor(count / screen_columns.length);
+    this.cursor_position.x += count % screen_columns.length;
+
+    if(this.cursor_position.x >= screen_columns.length){
+      this.cursor_position.x = 0;
+      this.cursor_position.y += 1;
+    }
+
+    if(this.cursor_position.y >= screen_rows.length){
+      this.cursor_position.x = screen_columns.length - 1;
+      this.cursor_position.y = screen_rows.length - 1;
+    }    
+  }
 };
 
 module.exports = CursorService;

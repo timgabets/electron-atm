3da8c67c384db622283b0bf87ec9105ed72f51f2
Tim Gabets <tim@gabets.ru>
2017-10-02 09:26:06 +0300

    opcode.setBufferValueAt()

--- spec/services/OpcodeSpec.js
+++ spec/services/OpcodeSpec.js
@@ -1,18 +1,83 @@
 describe("OperationCodeBufferService", function() {
   var OperationCodeBufferService = require('../../src/services/opcode.js');
+  var log;
 
   beforeEach(function() {
-    s = new OperationCodeBufferService();
+    log = {
+      info: function() {
+      },
+      error: function() {
+      }
+    };
+
+    s = new OperationCodeBufferService(log);
   });
 
-  describe('getTerminalKey() and getMasterKey()', function(){
-    beforeEach(function() {
-      s.setMasterKey('B6D55EABAD23BC4FD558F8D619A21C34');
-      s.setTerminalKey('DEADBEEFDEADBEEFDEADBEEFDEADBEEF');
+  describe('setBufferValueAt()', function(){
+    it('should set buffer value at position 0', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(0, 'A')).toBeTruthy();
+      expect(s.buffer).toEqual('A       ');
     });
 
-    it('should get terminal key', function(){
-      expect(s.getTerminalKey()).toEqual(['DEADBEEFDEADBEEFDEADBEEFDEADBEEF', '2AE358']);
+    it('should set buffer value at position 1', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(1, 'B')).toBeTruthy();
+      expect(s.buffer).toEqual(' B      ');
+    });
+
+    it('should set buffer value at position 2', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(2, 'D')).toBeTruthy();
+      expect(s.buffer).toEqual('  D     ');
+    });
+
+    it('should set buffer value at position 3', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(3, 'E')).toBeTruthy();
+      expect(s.buffer).toEqual('   E    ');
+    });
+
+    it('should set buffer value at position 4', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(4, 'F')).toBeTruthy();
+      expect(s.buffer).toEqual('    F   ');
+    });
+
+    it('should set buffer value at position 5', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(5, 'G')).toBeTruthy();
+      expect(s.buffer).toEqual('     G  ');
+    });
+
+    it('should set buffer value at position 6', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(6, 'H')).toBeTruthy();
+      expect(s.buffer).toEqual('      H ');
+    });
+
+    it('should set buffer value at position 7', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(7, 'I')).toBeTruthy();
+      expect(s.buffer).toEqual('       I');
+    });
+
+    it('should not change buffer value in case of index overflow', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(-1, 'H')).toBeFalsy();
+      expect(s.buffer).toEqual('        ');
+    });
+
+    it('should not change buffer value in case of index multiple-char-length string', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(1, 'AB')).toBeFalsy();
+      expect(s.buffer).toEqual('        ');
+    });
+
+    it('should not change buffer value to the values, other than [A-I]', function(){
+      expect(s.buffer).toEqual('        ');
+      expect(s.setBufferValueAt(3, 'J')).toBeFalsy();
+      expect(s.buffer).toEqual('        ');
     });
   });
 });
--- src/services/opcode.js
+++ src/services/opcode.js
@@ -1,5 +1,29 @@
-function OperationCodeBufferService(){
+function OperationCodeBufferService(log){
+  this.buffer = '        ';
 
+  /**
+   * [setBufferValueAt set this.opcode_buffer[position] with the value ]
+   * @param {[type]} position [description]
+   * @param {[type]} value    [description]
+   */
+  this.setBufferValueAt = function(position, value){
+    if(position < 0 || position > 7){
+      log.error('Error changing Operation Code Buffer at ' + position + '. Must be non-negative integer less than 8');
+      return false;
+    }
+
+    if(value.length !== 1 ){
+      log.error('Error changing Operation Code Buffer to ' + value + '. Must be single-char string');
+      return false;
+    }
+
+    if(value.charCodeAt(0) < 65 || // 'A'
+       value.charCodeAt(0) > 73) // 'I'
+      return false;
+    
+    this.buffer = this.buffer.substr(0, position) + value + this.buffer.substr(position + 1);
+    return true;
+  }
 }
 
 module.exports = OperationCodeBufferService;

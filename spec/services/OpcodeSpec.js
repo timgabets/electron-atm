88ea76588374b8ea65ebd80a30d7682bc4e4fa4a
Tim Gabets <tim@gabets.ru>
2017-10-02 09:37:46 +0300

    opcode.setOpCodeBuffer()

--- spec/services/OpcodeSpec.js
+++ spec/services/OpcodeSpec.js
@@ -80,4 +80,107 @@ describe("OperationCodeBufferService", function() {
       expect(s.buffer).toEqual('        ');
     });
   });
+
+  describe("setOpCodeBuffer()", function(){
+    it("should clear opcode buffer completely", function() {
+      /**
+       * Specifies bytes of Operation Code buffer to be cleared to graphic ‘space’. Each bit relates to a byte
+       * in the Operation Code buffer. If a bit is zero, the corresponding entry is cleared. If a bit is one, the
+       * corresponding entry is unchanged. 
+       */
+      s.buffer = 'HHHHHHHH'
+
+      var stateD ={ 
+        clear_mask: '000',  // 0000 0000
+        A_preset_mask: '000',
+        B_preset_mask: '000',
+        C_preset_mask: '000',
+        D_preset_mask: '000',
+        extension_state: '000' 
+      };
+
+      expect(s.buffer).toEqual('HHHHHHHH');
+      expect(s.setOpCodeBuffer(stateD)).toBeTruthy();
+      expect(s.buffer).toEqual('        ');
+    });
+
+    it("should left opcode buffer untouched", function() {
+      s.buffer = 'HHHHHHHH'
+      var stateD = { 
+        clear_mask: '255',  // 1111 1111
+        A_preset_mask: '000',
+        B_preset_mask: '000',
+        C_preset_mask: '000',
+        D_preset_mask: '000',
+        extension_state: '000' 
+      };
+
+      expect(s.buffer).toEqual('HHHHHHHH');
+      expect(s.setOpCodeBuffer(stateD)).toBeTruthy();
+      expect(s.buffer).toEqual('HHHHHHHH');
+    });
+
+    it("should clear only the left half of opcode buffer", function() {
+      s.buffer = 'BBBBBBBB'
+      var stateD = { 
+        clear_mask: '240', // 1111 0000
+        A_preset_mask: '000',
+        B_preset_mask: '000',
+        C_preset_mask: '000',
+        D_preset_mask: '000',
+        extension_state: '000' 
+      };
+
+      expect(s.buffer).toEqual('BBBBBBBB');
+      expect(s.setOpCodeBuffer(stateD)).toBeTruthy();
+      expect(s.buffer).toEqual('    BBBB');
+    });
+
+
+    it("should set opcode values with proper B and C characters", function() {
+      var stateD = { 
+        clear_mask: '000', 
+        A_preset_mask: '000', // 0000 0000
+        B_preset_mask: '042', // 0010 1010
+        C_preset_mask: '006', // 0000 0110 
+        D_preset_mask: '000', // 0000 0000
+        extension_state: '000' 
+      };
+
+      expect(s.buffer).toEqual('        ');
+      expect(s.setOpCodeBuffer(stateD)).toBeTruthy();
+      expect(s.buffer).toEqual(' CCB B  ');
+    });
+
+    it("should set opcode values characters from extension state", function() {
+      var stateD = { 
+        clear_mask: '000', 
+        A_preset_mask: '000', // 0000 0000
+        B_preset_mask: '000', // 0000 0000
+        C_preset_mask: '000', // 0000 0000
+        D_preset_mask: '000', // 0000 0000
+        extension_state: '000' 
+      };
+
+      var stateZ = { 
+        number: '037', 
+        type: 'Z',
+        description: 'Extension state',
+        entries: [ null, 'Z', 
+          '128', // F 1000 0000
+          '064', // G 0100 0000
+          '052', // H 0011 0100
+          '009', // I 0000 1001
+          'CDE', 
+          'FGH', 
+          'IJK', 
+          'LMN' ] 
+      };
+
+      expect(s.buffer).toEqual('        ');
+      expect(s.setOpCodeBuffer(stateD, stateZ)).toBeTruthy();
+      expect(s.buffer).toEqual('I HIHHGF');
+    });
+  });
+
 });
--- src/services/opcode.js
+++ src/services/opcode.js
@@ -23,7 +23,57 @@ function OperationCodeBufferService(log){
     
     this.buffer = this.buffer.substr(0, position) + value + this.buffer.substr(position + 1);
     return true;
+  };
+
+    /**
+   * [setOpCodeBuffer process the D state logic (Pre‐Set Operation Code Buffer)]
+   * @param {[state]} state [D-type state]
+   * @param {[extension_state]} state [Z-type state]
+   */
+  this.setOpCodeBuffer = function(state, extension_state){
+    /**
+     * Specifies bytes of Operation Code buffer to be cleared to graphic ‘space’. Each bit relates to a byte
+     * in the Operation Code buffer. If a bit is zero, the corresponding entry is cleared. If a bit is one, the
+     * corresponding entry is unchanged. 
+     */
+    var mask = state.clear_mask;
+    for(var bit = 0; bit < 8; bit++){
+      if((mask & Math.pow(2, bit)).toString() === '0')
+        this.setBufferValueAt(bit, ' ');
+    }
+
+    /**
+     * The buffer contains eight bytes. This entry sets the specified bytes to one of the values from keys[]. If a bit is one, the
+     * corresponding entry is set to keys[i]. If a bit is zero, the corresponding entry is unchanged.
+     */
+    var keys = ['A', 'B', 'C', 'D'];
+    ['A_preset_mask',
+     'B_preset_mask',
+     'C_preset_mask',
+     'D_preset_mask'
+     ].forEach( (element, i) => {
+        mask = state[element];
+        for(var bit = 0; bit < 8; bit++){
+          if((mask & Math.pow(2, bit)).toString() === Math.pow(2, bit).toString())
+            this.setBufferValueAt(bit, keys[i]);
+        }
+     });
+
+    if(extension_state && extension_state.entries){
+      var keys = [null, null, 'F', 'G', 'H', 'I'];
+      for(var i = 2; i < 6; i++){
+        mask = extension_state.entries[i];
+        for(var bit = 0; bit < 8; bit++){
+          if((mask & Math.pow(2, bit)).toString() === Math.pow(2, bit).toString())
+            this.setBufferValueAt(bit, keys[i]);
+        }
+       };
+    }
+
+    return true;
   }
+
+
 }
 
 module.exports = OperationCodeBufferService;

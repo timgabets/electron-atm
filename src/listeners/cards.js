47bb24faf7503e5432f5073a30bf3cf68794793f
Tim Gabets <tim@gabets.ru>
2017-10-04 18:47:14 +0300

    Fixed indentation in buildCardsList()

--- src/listeners/cards.js
+++ src/listeners/cards.js
@@ -24,41 +24,43 @@ $(function () {
    */
   function buildCardsList(){
     cards.getNames().forEach( (name) => {
+      //$(".list-group-item").remove();
+
       var card = cards.get(name);
       
       var record = '<a href="#" class="list-group-item">\
         <div class="row">\
           <div class="col-xs-1">';
 
-        if(card.scheme)
-          record += '<img id="scheme-logo" class="scheme-logo" src="img/schemes/' + card.scheme + '.png">';
-        record += '</div>';
+      if(card.scheme)
+        record += '<img id="scheme-logo" class="scheme-logo" src="img/schemes/' + card.scheme + '.png">';
+      record += '</div>';
 
-        record += '<div class="col-xs-2" title="Card number">' + card.name + '</div>'
+      record += '<div class="col-xs-2" title="Card number">' + card.name + '</div>'
 
-        record += '<div class="col-xs-1" title="FIT">XX</div>'
+      record += '<div class="col-xs-1" title="FIT">XX</div>'
 
-        record += '<div class="col-xs-1">'
-        if(card.PIN)
-          record += '<span class="badge" title="PIN">' + card.PIN + '</span>';
-        record += '</div>';
+      record += '<div class="col-xs-1">'
+      if(card.PIN)
+        record += '<span class="badge" title="PIN">' + card.PIN + '</span>';
+      record += '</div>';
 
-        record += '<div class="col-xs-1" title="Expiration Date">' + card.expiry_date + '</div>'
-        record += '<div class="col-xs-1" title="Service Code">' + card.service_code + '</div>'
-        record += '<div class="col-xs-1" title="PVK Index">' + card.pvki + '</div>'
-        record += '<div class="col-xs-1" title="PIN Verification Value">' + card.pvv + '</div>'
-        record += '<div class="col-xs-1" title="Card Verification Value">' + card.cvv + '</div>'
+      record += '<div class="col-xs-1" title="Expiration Date">' + card.expiry_date + '</div>'
+      record += '<div class="col-xs-1" title="Service Code">' + card.service_code + '</div>'
+      record += '<div class="col-xs-1" title="PVK Index">' + card.pvki + '</div>'
+      record += '<div class="col-xs-1" title="PIN Verification Value">' + card.pvv + '</div>'
+      record += '<div class="col-xs-1" title="Card Verification Value">' + card.cvv + '</div>'
         
-        record += '<div class="col-xs-1">' 
-        if(card.discretionary_data)
-          record += card.discretionary_data;
-        record += '</div>';
+      record += '<div class="col-xs-1">' 
+      if(card.discretionary_data)
+        record += card.discretionary_data;
+      record += '</div>';
               
-        record += '</div>';
-        record += '</a>';
+      record += '</div>';
+      record += '</a>';
 
-        $("#cards-page-cards-list").append(record);
-        $("#atm-page-cards-list").append('<option value="' + card.number + '">' + card.name + '</option>');
+      $("#cards-page-cards-list").append(record);
+      $("#atm-page-cards-list").append('<option value="' + card.number + '">' + card.name + '</option>');
     });
   };
 
@@ -107,6 +109,7 @@ $(function () {
       }
   
       ipc.send('ui-add-new-card', card);
+      buildCardsList();
     }
   });
 

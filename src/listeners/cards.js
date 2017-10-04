ea4c7296b10999519f95906270a7f610381c2d0f
Tim Gabets <tim@gabets.ru>
2017-10-04 16:12:35 +0300

    New card form moved from new-card.html to index.html

--- src/listeners/cards.js
+++ src/listeners/cards.js
@@ -9,3 +9,27 @@ let cards = new CardsService(settings, log);
 ipc.on('cards-add-new-card', (event, card) => {
   cards.add(card);
 })
+
+$(function () {
+  const electron = nodeRequire('electron')
+  const ipc = electron.ipcRenderer;
+  const settings = nodeRequire('electron-settings');
+  const CardsService = nodeRequire('./src/services/cards.js');
+
+  let cards = new CardsService(settings);
+
+  $("#add-new-card-form").validate();
+
+  var payment_scheme;
+  $('#cardnumber').blur(function(){
+    var cardnumber = $('#cardnumber').val();
+    if(cardnumber && cardnumber.length >= 16){
+      payment_scheme = cards.getPaymentScheme(cardnumber);
+      if(payment_scheme){
+        $("#scheme-logo").show();
+        $("#scheme-logo").attr('src', 'img/schemes/' + payment_scheme + '.png')
+      } else 
+        $("#scheme-logo").hide();
+    }
+  })
+});

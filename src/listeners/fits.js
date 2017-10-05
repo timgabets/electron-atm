489317b5c4baf92c03bb973f5256d5eb2c077980
Tim Gabets <tim@gabets.ru>
2017-10-05 12:35:16 +0300

    Appending FIT records

--- src/listeners/fits.js
+++ src/listeners/fits.js
@@ -9,7 +9,18 @@ let fits = new FITsService(settings, log);
 $(function () {
   function buildFITsList(){
     fits.get().forEach( (fit) => {
-      
+      fit_record = '<a href="#" class="list-group-item"><div class="row">';
+      fit_record += '<div class="col-xs-1"></div>';
+
+      fit_record += '<div class="col-xs-1">' + fit.PIDDX + '</div>';
+      fit_record += '<div class="col-xs-2">' + fit.PFIID + '</div>';
+      fit_record += '<div class="col-xs-1">' + fit.PSTDX + '</div>';
+      fit_record += '<div class="col-xs-1">' + fit.PAGDX + '</div>';
+      fit_record += '<div class="col-xs-1">' + fit.PMXPN + '</div>';
+
+      fit_record += '</div></a>';
+
+      $("#fits-list").append(fit_record);
     });
   };
   

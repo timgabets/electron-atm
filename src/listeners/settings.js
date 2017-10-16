821f21a6b1981dce94ca61cdcbe8327d3e7cbd25
Tim Gabets <tim@gabets.ru>
2017-10-16 11:18:27 +0300

    Saving settings

--- src/listeners/settings.js
+++ src/listeners/settings.js
@@ -1,10 +1,11 @@
 $(function(){
   var host = settings.get('host');
+  console.log(host);
 
   if(host){
     $('#settings-atm-host').val(host.ip);
     $('#settings-atm-port').val(host.port);
-    $('#settings-atm-luno').val(host.luno);
+    $('#settings-luno').val(host.luno);
   }
 
   var master_key = settings.get('master_key');
@@ -27,7 +28,7 @@ $(function(){
   }, 300);
 
   var comms_key = settings.get('pin_key');
-  var comms_key = settings.get('comms_key');
+  //var comms_key = settings.get('comms_key');
   if(comms_key){
     $('#settings-comms-key').val(comms_key);
     $('#settings-comms-key-cv').val(crypto.getKeyCheckValue(comms_key));
@@ -46,8 +47,27 @@ $(function(){
     }
   }, 300);
 
+  // Form
   setInterval(function(){
     if($('#settings-form').valid() )
       $('#settings-submit-button').attr('disabled', false)
-  }, 300)
+  }, 300);
+
+  $('#settings-form').submit(function(e) {
+    e.preventDefault();
+
+    // Saving host settings
+    var host = {
+      ip: $('#settings-atm-host').val(),
+      port: $('#settings-atm-port').val(),
+      luno: $('#settings-luno').val()
+    };
+    settings.set('host', host);
+
+    // Saving keys
+    settings.set('master_key', $('#settings-master-key').val());
+    settings.set('pin_key', $('#settings-comms-key').val());
+
+    console.log('Submitted');
+  });
 });

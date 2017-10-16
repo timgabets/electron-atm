320e45467770dc0a9d68972d2af5ec86f705bd95
Tim Gabets <tim@gabets.ru>
2017-10-16 19:22:16 +0300

    appendCard()

--- src/listeners/cards.js
+++ src/listeners/cards.js
@@ -2,13 +2,48 @@
  * Cards listener passes requests to/from CardsService
  */
 
-ipc.on('cards-add-new-card', (event, card) => {
-  cards.add(card);
-})
-
 $(function () {
   const electron = nodeRequire('electron')
   const ipc = electron.ipcRenderer;
+
+  /**
+   * [appendCard description]
+   * @param  {[type]} card [description]
+   * @return {[type]}      [description]
+   */
+  function appendCard(card){
+    var record = '<a href="#" class="list-group-item">\
+        <div class="row">\
+          <div class="col-xs-1">';
+
+    if(card.scheme)
+      record += '<img class="scheme-logo" src="img/schemes/' + card.scheme + '.png" title="' + card.scheme + ' Payment Scheme">';
+    record += '</div>';
+
+    record += '<div class="col-xs-2" title="Card number">' + card.name + '</div>';
+    record += '<div class="col-xs-1" title="Financial Institution ID">' + fits.getInstitutionByCardnumber(card.number) + '</div>'
+        
+    record += '<div class="col-xs-1">';
+    if(card.PIN)
+      record += '<span class="badge" title="PIN">' + card.PIN + '</span>';
+    record += '</div>';
+
+    record += '<div class="col-xs-1" title="Expiration Date">' + card.expiry_date + '</div>'
+    record += '<div class="col-xs-1" title="Service Code">' + card.service_code + '</div>'
+    record += '<div class="col-xs-1" title="PVK Index">' + card.pvki + '</div>'
+    record += '<div class="col-xs-1" title="PIN Verification Value">' + card.pvv + '</div>'
+    record += '<div class="col-xs-1" title="Card Verification Value">' + card.cvv + '</div>'
+        
+    record += '<div class="col-xs-1">' 
+    if(card.discretionary_data)
+      record += card.discretionary_data;
+    record += '</div>';
+              
+    record += '</div>';
+    record += '</a>';
+
+    $("#cards-page-cards-list").append(record);
+  }
 
   /**
    * [buildCardsList description]
@@ -17,43 +52,12 @@ $(function () {
   function buildCardsList(){
     cards.getNames().forEach( (name) => {
       var card = cards.get(name);
-
-      var record = '<a href="#" class="list-group-item">\
-        <div class="row">\
-          <div class="col-xs-1">';
-
-        if(card.scheme)
-          record += '<img class="scheme-logo" src="img/schemes/' + card.scheme + '.png" title="' + card.scheme + ' Payment Scheme">';
-        record += '</div>';
-
-        record += '<div class="col-xs-2" title="Card number">' + card.name + '</div>';
-        record += '<div class="col-xs-1" title="Financial Institution ID">' + fits.getInstitutionByCardnumber(card.number) + '</div>'
-        
-        record += '<div class="col-xs-1">';
-        if(card.PIN)
-          record += '<span class="badge" title="PIN">' + card.PIN + '</span>';
-        record += '</div>';
-
-        record += '<div class="col-xs-1" title="Expiration Date">' + card.expiry_date + '</div>'
-        record += '<div class="col-xs-1" title="Service Code">' + card.service_code + '</div>'
-        record += '<div class="col-xs-1" title="PVK Index">' + card.pvki + '</div>'
-        record += '<div class="col-xs-1" title="PIN Verification Value">' + card.pvv + '</div>'
-        record += '<div class="col-xs-1" title="Card Verification Value">' + card.cvv + '</div>'
-        
-        record += '<div class="col-xs-1">' 
-        if(card.discretionary_data)
-          record += card.discretionary_data;
-        record += '</div>';
-              
-        record += '</div>';
-        record += '</a>';
-
-        $("#cards-page-cards-list").append(record);
-
-        if(card.number === settings.get('last_used_card'))
-          $("#atm-page-cards-list").append('<option value="' + card.number + '" selected>' + card.name + '</option>')
-        else
-          $("#atm-page-cards-list").append('<option value="' + card.number + '">' + card.name + '</option>')        
+      appendCard(card);
+      
+      if(card.number === settings.get('last_used_card'))
+        $("#atm-page-cards-list").append('<option value="' + card.number + '" selected>' + card.name + '</option>')
+      else
+        $("#atm-page-cards-list").append('<option value="' + card.number + '">' + card.name + '</option>')        
     });
 
     if(cards.getNames().length > 0){
@@ -120,7 +124,9 @@ $(function () {
         discretionary_data: $('#discr-data').val(),
       }
   
-      ipc.send('ui-add-new-card', card);
+      if(cards.add(card)){
+
+      }
     }
   });
 

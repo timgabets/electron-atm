8d917a47d8ada6a6d429554d28c743da255fae04
Tim Gabets <tim@gabets.ru>
2017-10-20 09:32:41 +0300

    v1.2.0 Added 'ava' test framework that will replace 'jasmine'

---
+++ tests/controllers/trace.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+
+test('should return true', t => {
+  t.is(true, true)
+});
+
+/*
+import Trace from '../src/trace'
+
+var trace = new Trace()
+
+test('should dump empty data', t => {
+  t.is(trace.dump(''), '')
+});
+*/

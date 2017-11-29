a9645d47b623e4d3023204a007a178575ea1f590
Tim Gabets <tim@gabets.ru>
2017-11-29 08:56:57 +0300

    Added convertion script

---
+++ convert.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sed -i -e 's/    it/test/g' $1 
+sed -i -e 's/    })/})/g' $1 
+sed -i -e 's/      expect/  t.is/g' $1 
+sed -i -e 's/).toBeTruthy(/, true/g' $1 
+sed -i -e 's/).toBeFalsy(/, false/g' $1 
+sed -i -e 's/).toEqual(/, /g' $1 
+sed -i -e 's/function()/t =>/g' $1 
+

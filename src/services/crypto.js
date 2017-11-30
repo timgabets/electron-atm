400bfd722d2fc48f3de2c8701604fd652da17de0
Tim Gabets <tim@gabets.ru>
2017-11-30 12:40:08 +0300

    Applying linting rules to src/services/crypto.js

--- src/services/crypto.js
+++ src/services/crypto.js
@@ -1,33 +1,50 @@
 const Pinblock = require('pinblock');
 const des3 = require('node-cardcrypto').des;
 
-function CryptoService(settings, log){
+class CryptoService {
+  constructor(settings, log){
+    this.settings = settings;
+    this.log = log;
+    this.pinblock = new Pinblock();
+  
+    this.keys = {
+      master_key: {
+        key: '', 
+        check_value: ''},
+      pin_key: {
+        key: '', 
+        check_value: ''}
+    };
+
+    (settings.get('master_key')) ? this.setMasterKey(settings.get('master_key')) : this.setMasterKey('');
+    (settings.get('pin_key')) ? this.setTerminalKey(settings.get('pin_key')) : this.setTerminalKey('');
+  }
+  
   /**
    * [setMasterKey description]
    * @param {[type]} key [description]
    */
-  this.setMasterKey = function(key){
+  setMasterKey(key){
     this.keys.master_key.key = key;
-    settings.set('master_key', key);
-  };
+    this.settings.set('master_key', key);
+  }
 
-    /**
+  /**
    * [setTerminalKey description]
    * @param {[type]} key [description]
    */
-  this.setTerminalKey = function(key){
+  setTerminalKey(key){
     this.keys.pin_key.key = key;
-    settings.set('pin_key', key);
-  };
-
+    this.settings.set('pin_key', key);
+  }
 
   /**
    * [getKeyCheckValue description]
    * @param  {[type]} key [description]
    * @return {[type]}     [description]
    */
-  this.getKeyCheckValue = function(key){
-    var kcv = des3.ecb_encrypt(key, '00000000000000000000000000000000');
+  getKeyCheckValue(key){
+    let kcv = des3.ecb_encrypt(key, '00000000000000000000000000000000');
     if(kcv)
       return kcv.substr(0, 6);
     else
@@ -38,30 +55,28 @@ function CryptoService(settings, log){
    * [getTerminalKey description]
    * @return {[type]} [description]
    */
-  this.getTerminalKey = function(){
+  getTerminalKey(){
     return [this.keys.pin_key.key, this.getKeyCheckValue(this.keys.pin_key.key)];
-  };
+  }
 
 
   /**
    * [getMasterKey description]
    * @return {[type]} [description]
    */
-  this.getMasterKey = function(){
+  getMasterKey(){
     return [this.keys.master_key.key, this.getKeyCheckValue(this.keys.master_key.key)] ;
-  };
-
-
+  }
 
   /**
    * [dec2hex convert decimal string to hex string, e.g. 040198145193087203201076202216192211251240251237 to 28C691C157CBC94CCAD8C0D3FBF0FBED]
    * @param  {[type]} dec_string [decimal string ]
    * @return {[type]}            [hex string]
    */
-  this.dec2hex = function (dec_string){
-    var hex_string = '';
-    for(var i = 0; i < dec_string.length; i += 3){
-      var chunk = parseInt(dec_string.substr(i, 3)).toString(16);
+  dec2hex(dec_string){
+    let hex_string = '';
+    for(let i = 0; i < dec_string.length; i += 3){
+      let chunk = parseInt(dec_string.substr(i, 3), 10).toString(16);
       (chunk.length === 1) ? (hex_string = hex_string + '0' + chunk ) : hex_string += chunk;
     }
 
@@ -73,74 +88,57 @@ function CryptoService(settings, log){
    * @param {[type]} key_decimal [description]
    * @param {[type]} length      [description]
    */
-  this.setCommsKey = function(key_decimal, length){
-    var comms_key = this.dec2hex(key_decimal);
-    var expected_key_length = parseInt(length, 16) / 1.5;
+  setCommsKey(key_decimal, length){
+    let comms_key = this.dec2hex(key_decimal);
+    let expected_key_length = parseInt(length, 16) / 1.5;
 
-    if(comms_key.length !== expected_key_length)
-    {
-      log.error('Key length mismatch. New key has length ' + comms_key.length + ', but expected length is ' + expected_key_length);
+    if(comms_key.length !== expected_key_length){
+      this.log.error('Key length mismatch. New key has length ' + comms_key.length + ', but expected length is ' + expected_key_length);
       return false;
     }
 
-    log.info('New comms key received: ' + comms_key);
-    if(!this.keys.master_key.key)
-    {
-      log.error('Invalid master key: ' + this.keys.master_key.key);
+    this.log.info('New comms key received: ' + comms_key);
+    if(!this.keys.master_key.key){
+      this.log.error('Invalid master key: ' + this.keys.master_key.key);
       return false;
     }
 
     this.keys.pin_key.key = des3.ecb_decrypt(this.keys.master_key.key, comms_key);
-    log.info('New comms key value: ' + this.keys.pin_key.key);
-    settings.set('pin_key', this.keys.pin_key.key);
+    this.log.info('New comms key value: ' + this.keys.pin_key.key);
+    this.settings.set('pin_key', this.keys.pin_key.key);
     return true;
-  };
+  }
 
   /**
    * [getEncryptedPIN description]
    * @return {[type]}           [description]
    */
-  this.getEncryptedPIN = function(PIN_buffer, card_number){
+  getEncryptedPIN(PIN_buffer, card_number){
     if(this.keys.pin_key.key){
-      log.info('Clear PIN block:     [' + this.pinblock.get(PIN_buffer, card_number) + ']')
+      this.log.info('Clear PIN block:     [' + this.pinblock.get(PIN_buffer, card_number) + ']');
 
-      var encrypted_pinblock = des3.ecb_encrypt(this.keys.pin_key.key, this.pinblock.get(PIN_buffer, card_number));
-      log.info('Encrypted PIN block: [' + encrypted_pinblock + ']');
+      let encrypted_pinblock = des3.ecb_encrypt(this.keys.pin_key.key, this.pinblock.get(PIN_buffer, card_number));
+      this.log.info('Encrypted PIN block: [' + encrypted_pinblock + ']');
 
-      var atm_pinblock = this.pinblock.encode_to_atm_format(encrypted_pinblock);
-      log.info('Formatted PIN block: [' + atm_pinblock + ']');
+      let atm_pinblock = this.pinblock.encode_to_atm_format(encrypted_pinblock);
+      this.log.info('Formatted PIN block: [' + atm_pinblock + ']');
       
       return atm_pinblock;
-    } else
-    {
-      log.error('Terminal key is not set, unable to encrypt PIN block');
+    } else {
+      this.log.error('Terminal key is not set, unable to encrypt PIN block');
       return null;
     }
-  };
+  }
 
-  this.format = function(data){
-    var formatted = '';
-    for(var i = 0; i < data.length; i++){
+  format(data){
+    let formatted = '';
+    for(let i = 0; i < data.length; i++){
       if(i !== 0 && i % 4 === 0)
-        formatted += ' '
+        formatted += ' ';
       formatted += data[i];
     }
     return formatted;
   }
-
-  this.pinblock = new Pinblock();
-
-  this.keys = {
-    master_key: {
-      key: '', 
-      check_value: ''},
-    pin_key: {
-      key: '', 
-      check_value: ''}
-  };
-
-  (settings.get('master_key')) ? this.setMasterKey(settings.get('master_key')) : this.setMasterKey('');
-  (settings.get('pin_key')) ? this.setTerminalKey(settings.get('pin_key')) : this.setTerminalKey('');
-};
+}
 
 module.exports = CryptoService;

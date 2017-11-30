e6dd0475ea8c8ed3f2d81f1e19988dff5c5864fe
Tim Gabets <tim@gabets.ru>
2017-11-30 15:00:12 +0300

    v1.3.2 Fixed Issue#6 - builder.build() cognitive complexity refactored

--- src/controllers/builder.js
+++ src/controllers/builder.js
@@ -4,6 +4,131 @@ class Builder{
   }
 
   /**
+   * [buildSolicitedStatusTerminalState description]
+   * @param  {[type]} object [description]
+   * @return {[type]}        [description]
+   */
+  buildSolicitedStatusTerminalState(object){
+    let message = '';
+
+    switch(object.terminal_command){
+    case 'Send Configuration Information':
+      message += 'F\x1C1';
+      message += object.config_id + '\x1C';
+      message += object.hardware_fitness + '\x1C';
+      message += object.hardware_configuration + '\x1C';
+      message += object.supplies_status + '\x1C';
+      message += object.sensor_status + '\x1C';
+      message += object.release_number + '\x1C';
+      message += object.ndc_software_id;
+      break;
+
+    case 'Send Configuration ID':
+      message += 'F\x1C6' + object.config_id;
+      break;
+
+    case 'Send Supply Counters':
+      message += 'F\x1C2';
+      message += object.tsn;
+      message += object.transaction_count;
+      message += object.notes_in_cassettes;
+      message += object.notes_rejected;
+      message += object.notes_dispensed;
+      message += object.last_trxn_notes_dispensed;
+      message += object.card_captured;
+      message += object.envelopes_deposited;
+      message += object.camera_film_remaining;
+      message += object.last_envelope_serial;
+      break;
+
+    default:
+      break;
+    }
+
+    return message;
+  }
+
+  /**
+   * [buildSolicitedStatus description]
+   * @param  {[type]} object [description]
+   * @return {[type]}        [description]
+   */
+  buildSolicitedStatus(object){
+    let message = '';
+
+    switch(object.status_descriptor){
+    case 'Ready':
+      message += '9';
+      break;
+    case 'Command Reject':
+      message += 'A';
+      break;
+    case 'Specific Command Reject':
+      message += 'C';
+      break;
+    case 'Terminal State':
+      message += this.buildSolicitedStatusTerminalState(object);
+      break;
+    default:
+      message += 'A';
+    }
+
+    return message;
+  }
+
+  /**
+   * [buildSolicited description]
+   * @param  {[type]} object [description]
+   * @return {[type]}        [description]
+   */
+  buildSolicited(object){
+    let message = '';
+    if(object.message_subclass === 'Status'){
+      message += '2\x1C' + this.luno + '\x1C\x1C';
+      message += this.buildSolicitedStatus(object);
+    }
+    return message; 
+  }
+
+  /**
+   * [buildUnsolicited description]
+   * @param  {[type]} object [description]
+   * @return {[type]}        [description]
+   */
+  buildUnsolicited(object){
+    let message = '';
+    if(object.message_subclass === 'Transaction Request'){
+      message += '1';
+      message += '\x1C';
+      message += this.luno;
+      message += '\x1C';
+      message += '\x1C';
+      object.time_variant_number ? message += object.time_variant_number : message += '';
+      message += '\x1C';
+      message += object.top_of_receipt;
+      message += object.message_coordination_number;
+      message += '\x1C';
+      object.track2 ? message += object.track2 : message += '';
+      message += '\x1C';
+      object.track3 ? message += object.track3 : message += '';
+      message += '\x1C';
+      object.opcode_buffer ? message += object.opcode_buffer : message += '';
+      message += '\x1C';
+      object.amount_buffer ? message += object.amount_buffer : message += '';
+      message += '\x1C';
+      object.PIN_buffer ? message += object.PIN_buffer : message += '';
+      message += '\x1C';
+      object.buffer_B ? message += object.buffer_B : message += '';
+      message += '\x1C';
+      object.buffer_C ? message += object.buffer_C : message += '';
+      message += '\x1C';
+      object.track1 ? message += object.track1 : message += '';
+      message += '\x1C';
+    }
+    return message;
+  }
+
+  /**
    * [build description]
    * @param  {[type]} object [description]
    * @return {[type]}        [description]
@@ -17,94 +142,12 @@ class Builder{
     switch(object.message_class){
     case 'Solicited':
       message += 2;
-
-      switch(object.message_subclass){
-      case 'Status':
-        message += '2\x1C' + this.luno + '\x1C\x1C';
-        switch(object.status_descriptor){
-        case 'Ready':
-          message += '9';
-          break;
-        case 'Command Reject':
-          message += 'A';
-          break;
-        case 'Specific Command Reject':
-          message += 'C';
-          break;
-        case 'Terminal State':
-          switch(object.terminal_command){
-          case 'Send Configuration Information':
-            message += 'F\x1C1';
-            message += object.config_id + '\x1C';
-            message += object.hardware_fitness + '\x1C';
-            message += object.hardware_configuration + '\x1C';
-            message += object.supplies_status + '\x1C';
-            message += object.sensor_status + '\x1C';
-            message += object.release_number + '\x1C';
-            message += object.ndc_software_id;
-            break;
-          case 'Send Configuration ID':
-            message += 'F\x1C6' + object.config_id;
-            break;
-          case 'Send Supply Counters':
-            message += 'F\x1C2';
-            message += object.tsn;
-            message += object.transaction_count;
-            message += object.notes_in_cassettes;
-            message += object.notes_rejected;
-            message += object.notes_dispensed;
-            message += object.last_trxn_notes_dispensed;
-            message += object.card_captured;
-            message += object.envelopes_deposited;
-            message += object.camera_film_remaining;
-            message += object.last_envelope_serial;
-            break;
-          default:
-            break;
-          }
-          break;
-        default:
-          message += 'A';
-        }
-        break;
-      default:
-        break;
-      }
+      message += this.buildSolicited(object);
       break;
 
     case 'Unsolicited':
       message += 1;
-        
-      switch(object.message_subclass){
-      case 'Transaction Request':
-        message += '1';
-        message += '\x1C';
-        message += this.luno;
-        message += '\x1C';
-        message += '\x1C';
-        object.time_variant_number ? message += object.time_variant_number : message += '';
-        message += '\x1C';
-        message += object.top_of_receipt;
-        message += object.message_coordination_number;
-        message += '\x1C';
-        object.track2 ? message += object.track2 : message += '';
-        message += '\x1C';
-        object.track3 ? message += object.track3 : message += '';
-        message += '\x1C';
-        object.opcode_buffer ? message += object.opcode_buffer : message += '';
-        message += '\x1C';
-        object.amount_buffer ? message += object.amount_buffer : message += '';
-        message += '\x1C';
-        object.PIN_buffer ? message += object.PIN_buffer : message += '';
-        message += '\x1C';
-        object.buffer_B ? message += object.buffer_B : message += '';
-        message += '\x1C';
-        object.buffer_C ? message += object.buffer_C : message += '';
-        message += '\x1C';
-        object.track1 ? message += object.track1 : message += '';
-        message += '\x1C';
-        break;
-      }
+      message += this.buildUnsolicited(object);
       break;
 
     default:

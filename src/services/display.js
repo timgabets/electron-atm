cbe757d2e3f5b809725e30fe9818ace1a161d67b
Tim Gabets <tim@gabets.ru>
2017-11-30 20:04:33 +0300

    Using class DisplayService

--- src/services/display.js
+++ src/services/display.js
@@ -3,22 +3,24 @@ const CursorService = require('atm-cursor');
 const ScreenTextService = require('atm-screentext');
 const Trace = require('atm-trace');
 
-function DisplayService(screens, log){
-  this.screens = screens;
-  this.current_screen;
-  this.image_file;
-  this.cursor = new CursorService();
-  this.text = new ScreenTextService(this.cursor);
-  this.trace = new Trace();
+class DisplayService {
+  constructor(screens, log){
+    this.screens = screens;
+    this.log = log;
+    this.current_screen;
+    this.image_file;
+    this.cursor = new CursorService();
+    this.text = new ScreenTextService(this.cursor);
+    this.trace = new Trace();
+  }
 
-  this.setScreen = function(screen){
+  setScreen(screen){
     this.current_screen = screen;
 
     //log.info(this.trace.object(screen));
-
     this.current_screen.actions.forEach((element) => {
       if(element === 'clear_screen'){
-        this.text.init()        
+        this.text.init();        
       } else if( element['display_image'] ) {
         this.image_file = element['display_image'];
       } else if( element['move_cursor'] ){
@@ -26,52 +28,50 @@ function DisplayService(screens, log){
       } else if(element['add_text']){
         this.text.copy(element['add_text']);
       } else if(element['insert_screen']){
-        var subscreen = this.screens.get(element['insert_screen']);
+        let subscreen = this.screens.get(element['insert_screen']);
         this.setScreen(subscreen);
       }
     });
 
-    log.info('Screen changed to ' + this.current_screen.number);
+    this.log.info('Screen changed to ' + this.current_screen.number);
   }
 
-  this.setScreenByNumber = function(screen_number){
-    var screen = this.screens.get(screen_number)
-      if(screen){
-        this.setScreen(screen);
-      } else {
-        log.error('atm.setScreen(): unable to find screen ' + screen_number);
-      }
-  };
+  setScreenByNumber(screen_number){
+    let screen = this.screens.get(screen_number);
+    if(screen)
+      this.setScreen(screen);
+    else
+      this.log.error('atm.setScreen(): unable to find screen ' + screen_number);
+  }
 
-  this.getScreenNumber = function(){
+  getScreenNumber(){
     if(this.current_screen)
       return this.current_screen.number;
-  };
+  }
 
-  this.getScreen = function(){
+  getScreen(){
     return this.current_screen;
-  };
+  }
 
-  this.getImage = function(){
+  getImage(){
     return this.image_file;
-  };
+  }
 
   /**
    * [getHTML description]
    * @return {[type]} [description]
    */
-  this.getHTML = function(){
+  getHTML(){
     return this.text.getHTML();
-  };
-
+  }
 
   /**
    * [getText description]
    * @return {[type]} [description]
    */
-  this.getText = function(){
+  getText(){
     return this.text.get();
-  };
+  }
 
   /**
    * [insertText description]
@@ -79,13 +79,13 @@ function DisplayService(screens, log){
    * @param  {[type]}      [description]
    * @return {[type]}      [description]
    */
-  this.insertText = function(text, masking_symbol){
+  insertText(text, masking_symbol){
     if(masking_symbol)
       text = text.replace(/./gi, masking_symbol);
 
     this.text.init();
     this.text.put(text);
-  };
-};
+  }
+}
 
 module.exports = DisplayService;

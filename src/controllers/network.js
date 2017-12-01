56744ef12dfe41402ee40058146a1a284b28ead0
Tim Gabets <tim@gabets.ru>
2017-12-01 18:21:20 +0300

    class Network

--- src/controllers/network.js
+++ src/controllers/network.js
@@ -1,32 +1,35 @@
-// NodeJS net module
 const net = require('net');
-// trace routines
 const Trace = require('atm-trace');
 
-function Network(ipc, log) {
-  this.trace = new Trace();
-  //this.client = new net.Socket();
-  this.isConnected = false;
+class Network {
+  constructor(ipc, log) {
+    this.ipc = ipc;
+    this.log = log;
+
+    this.trace = new Trace();
+    this.isConnected = false;
+  }
 
   /**
    * [getOutgoingMessageLength get the Message Header Length]
    * @param  {[type]} data [binary data]
    * @return {[type]}      [two-bytes length, e.g. '\x00\x0a' for 10-byte data]
    */
-  this.getOutgoingMessageLength = function (data){
+  getOutgoingMessageLength(data){
     return String.fromCharCode(data.length / 256) + String.fromCharCode(data.length % 256);
-  };
+  }
 
   /**
    * [send description]
    * @param  {[type]} data [description]
    * @return {[type]}      [description]
    */
-  this.send = function (data){
+  send(data){
     let binary_data = Buffer(this.getOutgoingMessageLength(data) + data, 'binary');
     this.client.write(binary_data);
     this.trace.trace(binary_data, ' >> ' + binary_data.length + ' bytes sent:');
-  };
+  }
+
 
   /**
    * [toggleConnect description]
@@ -34,44 +37,35 @@ function Network(ipc, log) {
    * @param  {[type]} port [description]
    * @return {[type]}      [description]
    */
-  this.toggleConnect = function(host, port){
+  toggleConnect(host, port){
     if(!this.isConnected){
       this.trace.trace('', ' >> Connecting to ' + host + ':' + port );
 
       this.client = net.createConnection({ host: host, port: port }, () => {
         this.isConnected = true;
         this.trace.trace('', ' >> Connected' );
-        ipc.send('network-connection-status-change', this.isConnected);
+        this.ipc.send('network-connection-status-change', this.isConnected);
       });
 
       this.client.on('error', (e) => {
-        log.error(e);
+        this.log.error(e);
       });
 
       this.client.on('end', _ => {
         this.isConnected = false;
-        ipc.send('network-connection-status-change', this.isConnected);
-        log.warn('Connection closed');
+        this.ipc.send('network-connection-status-change', this.isConnected);
+        this.log.warn('Connection closed');
       });
 
       this.client.on('data', (data) => {
         this.trace.trace(data, ' << ' + data.length + ' bytes received:');
-        ipc.send('network-data-received', data);
+        this.ipc.send('network-data-received', data);
       });
     }else{
       this.client.end();
     }
-  };
+  }
 }
-
-/**
- * [send description]
- * @param  {[type]} message [description]
- * @return {[type]}      [description]
- */
-Network.prototype.send = function(message){
-  this.send(message);
-};
 
 module.exports = Network;
 
